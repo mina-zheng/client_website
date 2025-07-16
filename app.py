2b27ea16e5898b405dd6e4c6393ca697b28c79c5
MINA <minazheng505@gmail.com>
2025-07-16 14:36:07 -0400

    added index.css

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, flash, session
 from werkzeug.utils import secure_filename
 import os, json, uuid
 
 app = Flask(__name__)
 app.config['UPLOADS'] = "static/uploads"
 PRODUCTS_FILE = 'products.json'
+app.secret_key = "test"
 
 if os.path.exists(PRODUCTS_FILE):
     with open(PRODUCTS_FILE, 'r') as file:
@@ -24,6 +25,8 @@ def product_page(product_id):
 
 @app.route("/admin", methods = ["GET", "POST"])
 def admin():
+    if not session.get("ok"):
+        return redirect("/login")
     if request.method == "POST":
         name = request.form['name']
         price = request.form['price']
@@ -60,6 +63,25 @@ def delete_product(product_id):
     with open(PRODUCTS_FILE, 'w') as file:
         json.dump(products, file)
     return redirect("/admin")
+
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+    if request.method == "POST":
+        password = request.form['password']
+        if password == 'testpw':
+            session['ok'] = True
+            return redirect("/admin")
+        else:
+            flash("Wrong password. Try again.")
+            return redirect('/login')
+    return render_template('login.html')
+
+@app.route("/login-redirect")
+def home():
+    if session['ok']: 
+        return redirect("/admin")
+    else:
+        return redirect("/login")     
 
 if __name__ == '__main__':
     os.makedirs(app.config['UPLOADS'], exist_ok = True)
