9e433886a428f85549138e7e9e7ccbb4204ff5ab
MINA <minazheng505@gmail.com>
2025-07-29 19:01:22 -0400

    added flask login and env variables

--- app.py
+++ app.py
@@ -3,15 +3,28 @@ from werkzeug.utils import secure_filename
 import os, json, uuid
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects.postgresql import ARRAY
+from flask_login import LoginManager, UserMixin, login_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 PRODUCTS_FILE = 'products.json'
-app.secret_key = "test"
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Snakehead505@localhost/postgres'
+load_dotenv()
+print(os.environ.get("SQL_URL"))
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQL_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = os.environ.get("SECRET_KEY")
 
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+class Users(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.String, primary_key = True)
+    password = db.Column(db.String)
 
 class Product(db.Model):
     __tablename__ = 'products'
@@ -30,6 +43,20 @@ class Product(db.Model):
         self.images = images
         self.itemtype = itemtype
 
+"""@app.route("/create-user")
+def create_user():
+    if Users.query.get("1"):
+        return "User already exists."
+    hashed_pw = generate_password_hash(os.environ.get("ADMIN_PASSWORD"))
+    user = Users(id="1", password=hashed_pw)
+    db.session.add(user)
+    db.session.commit()
+    return "User created!"""
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(user_id)
+
 @app.route("/test-db") #test db connection
 def test_db():
     products = Product.query.all()
@@ -46,9 +73,10 @@ def product_page(product_id):
     return render_template("product.html", product = product)
 
 @app.route("/admin", methods = ["GET", "POST"])
+@login_required
 def admin():
-    if not session.get("ok"):
-        return redirect("/login")
+    print("Current user:", current_user)
+    print("Is authenticated?", current_user.is_authenticated)
     if request.method == "POST":
         name = request.form['name']
         price = request.form['price']
@@ -72,6 +100,7 @@ def admin():
     return render_template('admin.html', products = products)
 
 @app.route("/delete/<product_id>", methods = ["POST"])
+@login_required
 def delete_product(product_id):
     product = Product.query.get(product_id)
     db.session.delete(product)
@@ -82,20 +111,14 @@ def delete_product(product_id):
 def login():
     if request.method == "POST":
         password = request.form['password']
-        if password == 'testpw':
-            session['ok'] = True
+        user = Users.query.filter_by(id = '1').first()
+        if check_password_hash(user.password, password):
+            login_user(user)
             return redirect("/admin")
         else:
             flash("Wrong password. Try again.")
             return redirect('/login')
     return render_template('login.html')
-
-@app.route("/login-redirect")
-def home():
-    if session['ok']: 
-        return redirect("/admin")
-    else:
-        return redirect("/login")  
 
 @app.route("/crystals")
 def item1():
